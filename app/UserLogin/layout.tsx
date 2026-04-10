63e817231af9264a98c7e63cac9931dcf1c19e80
nadxhielijuarez <nadxhielijuarez@gmail.com>
2026-04-10 14:11:09 -0500

    starting with userlogin for blog input data ui

---
+++ app/UserLogin/layout.tsx
@@ -0,0 +1,7 @@
+export default function UserLoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ app/UserLogin/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { JSX } from "react";
+
+export default function UserLogin(): JSX.Element {
+    return <Button>Login</Button>;
+
+}
+

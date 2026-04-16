7c1a1b4a3206da3da44a35a9c91112f4872aaf77
nadxhielijuarez <nadxhielijuarez@gmail.com>
2026-04-16 17:11:57 -0500

    finished

--- app/components/BlogArticle.tsx
+++ app/components/BlogArticle.tsx
@@ -1,8 +1,8 @@
-import React, {JSX}from 'react';
+import React, { JSX } from 'react';
 import CardTagList from '../components/CardTagList';
 import '../css/article_project_landing.css';
+import '../css/article_post.css';
 import '../css/original.css';
-import Image from 'next/image';
 
 type BlogArticle = {
     title: string;
@@ -14,13 +14,17 @@ type BlogArticle = {
 
 export default function BlogArticle({ title,image, content, date, tags }: BlogArticle): JSX.Element{
     return (
-        <>
-           
-      <img className="HobbyImg" src={image} alt={title} />
-            <h1>{title}</h1>
-            <p>{content}</p>
-            <p>{date}</p>
+        <div className="BlogArticle">
+            <img className="BlogArticle-image" src={image} alt={title} />
+            <div className="BlogArticle-title">{title}</div>
+            {date ? <p className="BlogArticle-date">{date}</p> : null}
+            {content ? (
+                <div
+                    className="BlogArticle-content"
+                    dangerouslySetInnerHTML={{ __html: content }}
+                />
+            ) : null}
             <CardTagList tags={tags} className="BlogArticle-Tags" />
-        </>
+        </div>
     );
 }
